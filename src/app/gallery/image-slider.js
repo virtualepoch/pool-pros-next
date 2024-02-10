8f7a2ebb622f5f096284f0725e5d473fc434cfe2
Virtual Epoch <cdk11235@gmail.com>
2024-02-09 22:04:11 -0700

    more swiper testing

--- src/app/gallery/image-slider.js
+++ src/app/gallery/image-slider.js
@@ -2,17 +2,17 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
 export const ImageSlider = ({ styles, images }) => {
   const [imageIndex, setImageIndex] = useState(0);
 
-  const [btnLeftHovered, setBtnLeftHovered] = useState(false);
-  const [btnRightHovered, setBtnRightHovered] = useState(false);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   const [imgBtnsTrans, setImgBtnsTrans] = useState();
 
@@ -28,81 +28,63 @@ export const ImageSlider = ({ styles, images }) => {
         Skip image slider controls
       </a>
 
-      <div>
-        <Swiper
-          className="swiper_container"
-          // className={styles.wrapperImages}
-          loop
-          grabCursor
-          centeredSlides
-          slidesPerView="auto"
-          effect="coverflow"
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
-          pagination={{ el: ".swiper-pagination", clickable: true }}
-          navigation={{
-            nextEl: ".swiper-btn-prev-slide",
-            prevEl: ".swiper-btn-next-slide",
-            clickable: true,
-          }}
-        >
-          {images.map((images, index) => (
-            <SwiperSlide key={images.url} className="swiper-slide">
-              <Image
-                className="swiper-slide-img"
-                width={640}
-                height={480}
-                src={images.url}
-                alt={images.alt}
-              />
-            </SwiperSlide>
-          ))}
-
-          <div className="swiper-pag-container">
-            <button
-              className="swiper-btn swiper-btn-prev-slide"
-              aria-label="View previous image"
-              onTouchStart={() => setBtnLeftHovered(true)}
-              onTouchEnd={() => setBtnLeftHovered(false)}
-            />
-
-            <div className="swiper-pagination" />
-
-            <button
-              className="swiper-btn swiper-btn-next-slide"
-              aria-label="View next image"
-              onTouchStart={() => setBtnRightHovered(true)}
-              onTouchEnd={() => setBtnRightHovered(false)}
-            />
-          </div>
-        </Swiper>
-      </div>
-
-      {/* <div
-        className={styles.wrapperBtnsImg}
+      <Swiper
+        className="swiper_container"
+        // className={styles.wrapperImages}
         style={{
-          transform: `translateY(${imgBtnsTrans ? 0 : 100}%)`,
-          opacity: imgBtnsTrans ? 1 : 0,
+          "--swiper-navigation-color": "#0ffa",
+          "--swiper-pagination-color": "#0ffa",
         }}
+        loop
+        grabCursor
+        navigation
+        centeredSlides
+        watchSlidesProgress
+        slidesPerView="auto"
+        effect="coverflow"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[EffectCoverflow, FreeMode, Navigation, Thumbs]}
       >
-        {images.map((images, index) => (
-          <button
-            key={index}
-            className={styles.btnImg}
-            aria-label={`View image ${index + 1}`}
-            onClick={() => setImageIndex(index)}
-            style={{
-              background: `url(${images.url}) center / cover no-repeat`,
-              transform: `scale(${index === imageIndex ? 1.2 : 1})`,
-              border: index === imageIndex && "2px solid #0ff",
-            }}
-          />
+        {images.map((images) => (
+          <SwiperSlide key={images.url}>
+            <Image
+              className="swiper-slide-img"
+              width={640}
+              height={480}
+              src={images.url}
+              alt={images.alt}
+            />
+          </SwiperSlide>
         ))}
-      </div> */}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView="auto"
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="swiper-thumbnails"
+      >
+        {images.map((images) => (
+          <SwiperSlide key={images.url} className="swiper-thumbnails-slide">
+            <Image
+              className="swiper-thumbnails-img"
+              width={640}
+              height={480}
+              src={images.url}
+              alt={images.alt}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div id="after-image-slider-controls" />
     </section>
