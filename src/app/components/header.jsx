8a8d16db879de40b06480ae52c3d8870e343f105
Virtual Epoch <cdk11235@gmail.com>
2024-02-21 15:55:30 -0700

    t

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -2,21 +2,22 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, FreeMode, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
 
 const images = [
-  { src: "/t/1.png" },
+  { src: "/globals/site-logo.png" },
+  { src: "/t/1p.png" },
+  { src: "/t/1.webp" },
+  { src: "/t/2p.png" },
   { src: "/t/2.webp" },
-  { src: "/t/3.png" },
+  { src: "/t/3.webp" },
   { src: "/t/4.webp" },
-  { src: "/t/5.png" },
-  { src: "/t/6.webp" },
-  { src: "/t/7.png" },
-  { src: "/t/8.png" },
-  { src: "/t/9.png" },
-  { src: "/t/10.png" },
-  { src: "/t/11.png" },
-  { src: "/t/12.png" },
-  { src: "/t/13.png" },
+  { src: "/t/aa.webp" },
 ];
 
 export const Header = () => {
@@ -26,33 +27,47 @@ export const Header = () => {
     <>
       {pathname != "/services" && (
         <header className="header">
-          <Link href="/" className="idahomeserv-logo-link">
-            <Image
-              priority
-              className="idahomeserv-logo"
-              src="/globals/site-logo.png"
-              alt="Boise Pool Pros Logo"
-              width={800}
-              height={432}
-            />
-          </Link>
+          <Link href="/" className="idahomeserv-logo-link"></Link>
 
-          <hr />
-          {images.map((images, index) => (
-            <>
-              <div className="logo-testing-number">#{index + 1}</div>
-              <Image
-                priority
-                className="logo-testing"
-                src={images.src}
-                alt="Boise Pool Pros Logo"
-                width={400}
-                height={400}
-              />
-
-              <hr />
-            </>
-          ))}
+          <Swiper
+            className="swiper-logo-test"
+            style={{
+              "--swiper-navigation-color": "#0ffa",
+            }}
+            loop
+            // speed={500}
+            grabCursor
+            navigation
+            centeredSlides
+            watchSlidesProgress
+            slidesPerView="auto"
+            initialSlide={1}
+            effect="coverflow"
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 200,
+              modifier: 4,
+              slideShadows: false,
+            }}
+            modules={[EffectCoverflow, FreeMode, Navigation]}
+          >
+            {images.map((images, index) => (
+              <SwiperSlide key={images.url}>
+                <div className="logo-testing-wrap">
+                  <div className="logo-testing-number">#{index + 1}</div>
+                  <Image
+                    priority
+                    className="logo-testing"
+                    src={images.src}
+                    alt="Boise Pool Pros Logo"
+                    width={400}
+                    height={400}
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
 
           <h2 className="header-phone-number">(208)475-4344</h2>
         </header>
