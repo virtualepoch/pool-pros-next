466f1c75aacb6740525a88a5d693357a136773e3
Virtual Epoch <cdk11235@gmail.com>
2024-03-06 16:35:45 -0700

    layout update

--- src/app/services/components/landing.jsx
+++ src/app/services/components/landing.jsx
@@ -3,10 +3,10 @@ import { BtnAndModalContact } from "../../components/ui/btn-and-modal-contact";
 export const Landing = ({ styles }) => {
   return (
     <div className={styles.landing}>
-      <h2 className={styles.heroStatement}>
+      <h1 className={styles.heroStatement}>
         Our Professional Pool Services are designed to ensure your pool remains
         a pristine oasis of leisure and luxury.
-      </h2>
+      </h1>
 
       <BtnAndModalContact addedClass={styles.btnContact} />
     </div>
