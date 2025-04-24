4ff1464b4792674199a2136e8fa38f2333bbdb67
Virtual Epoch <cdk11235@gmail.com>
2025-04-24 07:48:32 -0600

    email address update / contact btn testing

--- src/app/components/links/link-email.jsx
+++ src/app/components/links/link-email.jsx
@@ -1,6 +1,6 @@
 export const LinkEmail = () => {
   return (
-    <a className="contact-link email" href="mailto:idahomeserv@gmail.com">
+    <a className="contact-link email" href="mailto:boisepoolpros@gmail.com">
       <p className="contact-link-text">Email</p>
       <div className="icon email" />
     </a>
